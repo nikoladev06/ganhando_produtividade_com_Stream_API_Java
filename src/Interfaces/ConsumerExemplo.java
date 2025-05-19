eb08b5ce6a53911a652b700556a4084e75e5ae5e
nikoladev06 <nikoladev06@gmail.com>
2025-05-19 20:39:13 -0300

    feat: adicionado descrição no exemplo de Consumer
    Signed-off-by: nikoladev06 <nikoladev06@gmail.com>

--- src/Interfaces/ConsumerExemplo.java
+++ src/Interfaces/ConsumerExemplo.java
@@ -4,21 +4,19 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
+/**
+ * Representa uma operação que aceita um argumento do tipo T e não retorna resultado.
+ * É utilizada principalmente para realizar ações, ou efeitos colaterais nos elementos do Stream sem modificar, ou retornar um valor.
+ */
+
 public class ConsumerExemplo {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        /* Método mais longo
         Consumer<Integer> imprimirNumerosPares = numero -> {
             if (numero % 2 == 0) System.out.println(numero);
         };
 
         numeros.stream().forEach(imprimirNumerosPares);
-         */
-
-        //Método mais correto/curto com lambda:
-        numeros.stream().forEach(n -> {
-            if (n % 2 == 0) System.out.println(n);
-        });
     }
 }
